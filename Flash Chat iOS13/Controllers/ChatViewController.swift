da04e90f1964e1347729a04024bbe5f742790ddc
sunnyle13 <4anythingbutwork@gmail.com>
2022-10-02 00:11:04 -0400

    Populated chat tableview and created a custom tableviewcell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageTextfield: UITextField!
+	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var messageTextfield: UITextField!
+	
+	var messages: [Message] = [
+		Message(sender: "test@test.com", body: "Hey!"),
+		Message(sender: "test1@test.com", body: "Hello!"),
+		Message(sender: "test2@test.com", body: "What's up?")
+	]
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		tableView.dataSource = self
+		title = K.appName
+		navigationItem.hidesBackButton = true
+		
+		// Register cell nib
+		tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+	}
+	
+	@IBAction func sendPressed(_ sender: UIButton) {
+	}
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+		do {
+			try Auth.auth().signOut()
+			navigationController?.popToRootViewController(animated: true)
+		} catch let signOutError as NSError {
+			print("Error signing out: %@", signOutError)
+		}
+	}
+}
 
-    }
-    
-    @IBAction func sendPressed(_ sender: UIButton) {
-    }
-    
-
+// MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return messages.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+		cell.label.text = messages[indexPath.row].body
+		return cell
+	}
 }
