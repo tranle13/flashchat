9249cad32611bcd6f6429dd0e45339e791928dd6
sunnyle13 <4anythingbutwork@gmail.com>
2022-10-01 22:20:44 -0400

    Added CLTypingLabel and Firebase pods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,22 +1,15 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
-    }
+	@IBOutlet weak var titleLabel: CLTypingLabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		titleLabel.text = "⚡️FlashChat"
+	}
     
 
 }
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'Flash Chat iOS13' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flash Chat
+  pod 'CLTypingLabel', '~> 0.4.0'
+
+	# Add the pods for any other Firebase products you want to use in your app
+	pod 'FirebaseAuth'
+	pod 'FirebaseFirestore'
+
+end
